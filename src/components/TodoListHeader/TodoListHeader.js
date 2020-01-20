87321192f7a7dc61bf0f54d33e396056d3b67aed
vgeroim <vgeroim.qa@gmail.com>
2020-01-20 18:06:33 +0000

    display error border if task input is empty

--- src/components/TodoListHeader/TodoListHeader.js
+++ src/components/TodoListHeader/TodoListHeader.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './TodoListHeader.css'
 
 class TodoListHeader extends React.Component {
   constructor(props) {
@@ -6,18 +7,45 @@ class TodoListHeader extends React.Component {
     this.newTaskTitleRef = React.createRef();
   }
 
+  state = {
+    error: false
+  }
+
   onAddTaskClick = () => {
     let newText = this.newTaskTitleRef.current.value
-    this.props.addTask(newText);
-    this.newTaskTitleRef.current.value = "";
+    if (newText === ""){
+      this.setState({
+        error: true
+      })
+
+    } else {
+      this.setState({
+        error: false
+      })
+      this.props.addTask(newText);
+      this.newTaskTitleRef.current.value = "";
+    }
+  }
+
+  onInputChange = () => {
+    this.setState({
+      error: false
+    })
   }
 
   render = () => {
+    let classForInput = this.state.error === true ? "error" : ""
+
     return(
       <div className="todoList-header">
         <h3 className="todoList-header__title">What to Learn</h3>
         <div className="todoList-newTaskForm">
-          <input ref={this.newTaskTitleRef} type="text" placeholder="New task name"/>
+          <input ref={this.newTaskTitleRef} 
+                 type="text" 
+                 placeholder="New task name"
+                 className={classForInput}
+                 onChange={this.onInputChange}
+          />
           <button onClick={this.onAddTaskClick}>Add</button>
         </div>
       </div>
